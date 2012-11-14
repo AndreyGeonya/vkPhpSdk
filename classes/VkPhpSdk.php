649cd33c14bf20478ff168d76d86fc4f453f8e96
kexek <mail@kexek.me>
2012-11-14 22:34:52 +0700

    updating vk domains

--- classes/VkPhpSdk.php
+++ classes/VkPhpSdk.php
@@ -48,8 +48,8 @@ class VkPhpSdk
 	 * Maps aliases to Vkontakte domains.
 	 */
 	public static $domainMap = array(
-		'api' => 'https://api.vkontakte.ru/method/',
-		'www' => 'http://www.vkontakte.ru/',
+		'api' => 'https://api.vk.com/method/',
+		'www' => 'http://www.vk.com/',
 	);
 		
 	private $_accessToken;
